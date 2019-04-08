30665e9d58305b18b1302f7368f13688a3b7387f
joshuagottardogalvani <gottardo.joshua@iisgalvanimi.edu.it>
2019-04-08 07:46:06 +0000

    Reactive Forms

---
+++ form-demo/src/app/form-demo/form-demo.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'app-form-demo',
+  templateUrl: './form-demo.component.html',
+  styleUrls: ['./form-demo.component.css']
+})
+
+export class FormDemoComponent implements OnInit {
+
+  myForm: FormGroup;
+
+  constructor(fb: FormBuilder) {
+    this.myForm = fb.group({
+      'sku': ['ABC123']
+    });
+  }
+
+  ngOnInit() {
+  }
+
+  onSubmit(value: string): void {
+    console.log('you submitted value: ', value);
+  }
+}
