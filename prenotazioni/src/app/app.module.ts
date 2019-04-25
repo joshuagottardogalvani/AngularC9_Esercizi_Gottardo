740616a568636c3ea01eee3c1f632c21754a5553
joshuagottardogalvani <gottardo.joshua@iisgalvanimi.edu.it>
2019-04-25 13:46:11 +0000

    creazione componenti

--- prenotazioni/src/app/app.module.ts
+++ prenotazioni/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PrenotazioniComponent } from './prenotazioni/prenotazioni.component';
+import { ViewPrenotazioniComponent } from './view-prenotazioni/view-prenotazioni.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PrenotazioniComponent,
+    ViewPrenotazioniComponent
   ],
   imports: [
     BrowserModule
---
+++ prenotazioni/src/app/view-prenotazioni/view-prenotazioni.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-view-prenotazioni',
+  templateUrl: './view-prenotazioni.component.html',
+  styleUrls: ['./view-prenotazioni.component.sass']
+})
+export class ViewPrenotazioniComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
