bfe986cd29267af45dcfba9be6dd8bcf658327ff
joshuagottardogalvani <gottardo.joshua@iisgalvanimi.edu.it>
2019-04-25 18:46:50 +0000

    5

--- prenotazioni/src/app/app.module.ts
+++ prenotazioni/src/app/app.module.ts
@@ -3,13 +3,11 @@ import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { PrenotazioniComponent } from './prenotazioni/prenotazioni.component';
 import { ViewPrenotazioneComponent } from './view-prenotazione/view-prenotazione.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PrenotazioniComponent,
     ViewPrenotazioneComponent
   ],
   imports: [
