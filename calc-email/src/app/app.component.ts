e31487664f8e2b369cae47b82be3538d7fbe0d1e
joshuagottardogalvani <gottardo.joshua@iisgalvanimi.edu.it>
2019-05-03 07:44:39 +0000

    calcolatrice

--- calc-email/src/app/app.component.ts
+++ calc-email/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'calc-email';
+  
+  myForm: FormGroup;
+  risultato: Number;
+
+  constructor(fb: FormBuilder) {
+    this.myForm = fb.group({'numero1': ['1', Validators.required], 'numero2': ['2', Validators.required]
+    });
+  }
+
+  somma() {
+   this.risultato = Number(this.myForm.controls['numero1'].value) + Number(this.myForm.controls['numero2'].value);
+   return false;
+  }
+
+  sottrazione() {
+   this.risultato = Number(this.myForm.controls['numero1'].value) - Number(this.myForm.controls['numero2'].value);
+   return false;
+  }
+
+  moltiplicazione() {
+   this.risultato = Number(this.myForm.controls['numero1'].value) * Number(this.myForm.controls['numero2'].value);
+   return false;
+  }
+  
+  divisione() {
+   this.risultato = Number(this.myForm.controls['numero1'].value) / Number(this.myForm.controls['numero2'].value);
+   return false;
+  }
+
 }
--- calc-email/src/app/ris-component/ris-component.component.ts
+++ calc-email/src/app/ris-component/ris-component.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-ris-component',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./ris-component.component.css']
 })
 export class RisComponentComponent implements OnInit {
+
+  @Input() risultato: Number;
 
   constructor() { }
 
