9e99fe40d3a95b341978c99c36bbda4097cd1882
joshuagottardogalvani <gottardo.joshua@iisgalvanimi.edu.it>
2019-05-19 19:47:56 +0000

    compravendita

--- compravendita/src/app/mock-posts.ts
+++ compravendita/src/app/mock-posts.ts
@@ -3,6 +3,5 @@ import { Post } from './post';
 export const Posts: Post[] = [
   { nome:'Maglietta', tipo:'abbigliamento', prezzo:5, descrizione:'Maglietta rossa con logo bianco', nomeUtente:'Sara', cognomeUtente:'Villa', username:'sara.villa'},
   { nome:'Bicicletta X40', tipo:'veicolo', prezzo:125, descrizione:'Bicicletta Vintage', nomeUtente:'Camilla', cognomeUtente:'Paglia', username:'cpaglia'},
-  { nome:'Tavolo', tipo:'arredamento', prezzo:55, descrizione:'Tavolo in legno di quercia', nomeUtente:'Mario', cognomeUtente:'Novembre', username:'marion'},
-  { nome:'Penna Stilografica', tipo:'cartoleria', prezzo:20, descrizione:'Penna stilografica ad inchiostro nero', nomeUtente:'Sara', cognomeUtente:'Villa', username:'sara.villa'}
+  { nome:'Tavolo', tipo:'arredamento', prezzo:55, descrizione:'Tavolo in legno di quercia', nomeUtente:'Mario', cognomeUtente:'Novembre', username:'marion'}
 ];
