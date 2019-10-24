a1c6efe10008db8a3871f5d84e41d4209a8bfe20
joshuagottardogalvani <gottardo.joshua@iisgalvanimi.edu.it>
2019-10-24 11:50:16 +0000

    PUG 2.1

--- ESEMPIO_PUG/app.js
+++ ESEMPIO_PUG/app.js
@@ -1,14 +1,17 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const people = require('./people.json'); //Copia il file people.json dentro la variabile people
+const app = express();
 
 app.set('view engine', 'pug');
-
-app.get('/', function (req, res) {
-  res.render('index');
-});
-
 app.use(express.static(__dirname + '/public'));
 
+app.get('/', (req, res) => {
+  res.render('index', {
+    title: 'Homepage',
+    people: people.profiles //Passa il vettore profiles alla pagina index.pug
+  });
+});
+
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+ console.log('Example app listening on port 3000!');
 });
