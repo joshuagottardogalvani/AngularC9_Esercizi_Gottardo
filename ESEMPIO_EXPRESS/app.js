00e019250cd37e53154613079eeb6fca762c8aac
joshuagottardogalvani <gottardo.joshua@iisgalvanimi.edu.it>
2019-11-05 12:57:41 +0000

    node

--- ESEMPIO_EXPRESS/app.js
+++ ESEMPIO_EXPRESS/app.js
@@ -1,8 +1,23 @@
 var express = require('express');
 var app = express();
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+const path = require('path');
+
+app.get('/',function(req,res){
+ res.sendFile(path.join(__dirname+'/index.html'));
 });
+
+app.get('/about',function(req,res){
+ res.sendFile(path.join(__dirname+'/about.html'));
+});
+
+app.get('/sitemap',function(req,res){
+ res.sendFile(path.join(__dirname+'/sitemap.html'));
+});
+
+app.get('/api/products',function(req,res){
+ res.sendFile(path.join(__dirname+'/products.json'));
+});
+
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
